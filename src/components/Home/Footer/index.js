80fbbd82e257a3272f5e34cd9fb2c825c7ba8636
Tomasz Stefaniuk <t.stefaniuk@icloud.com>
2020-10-26 15:16:37 +0100

    Mobile footer.

--- src/components/Home/Footer/index.js
+++ src/components/Home/Footer/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import './Footer.scss'
 
 
 const Footer = () => {
   return (
-    <div>
-      Footer!!!
-    </div>
+    <footer className='footer'>
+      <div className='sectionOne'>
+        <span>About</span>
+        <span>Team</span>
+        <span>Contact</span>
+      </div>
+      <div className='sectionTwo'>
+        <span>2020© </span>
+        <span>esemcode - Tomasz Stefaniuk</span>
+      </div>
+    </footer>
   )
 }
 
